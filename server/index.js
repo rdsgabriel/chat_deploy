9bc1fdef570738ad846c1c931e4e816088f6b3c5
rdsgabriel <gabriel.rodrigues.ismart@gmail.com>
2023-01-27 16:49:04 -0300

    cors

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://chatdeploy-production.up.railway.app/",
+    origin: "https://chatdeploy-production.up.railway.app",
     methods: ['GET', 'POST'],
   }
 });
