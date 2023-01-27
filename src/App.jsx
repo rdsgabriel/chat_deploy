a82fd79a6a16f1539de3f91becc4cf35ba62bab8
rdsgabriel <gabriel.rodrigues.ismart@gmail.com>
2023-01-27 17:31:48 -0300

    testanduu

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import './App.css'
 import Chat from './Chat'
 
-const socket = io.connect('mongodb://mongo:HVYaq85NgPPycgPVtixr@containers-us-west-96.railway.app:7963')
+const socket = io.connect('https://chatdeploy-production.up.railway.app')
 
 
 function App() {
